6039621db21532e4417d797a17ff358e5b507eb1
progmamun <mamun333089@gmail.com>
2023-07-26 12:21:45 +0600

    page-base-routing

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 ## Next News Portal
+
+### run this project:
+
+- npm i
+- npm run dev
+- npm run json-server
+
+### package:
+
+- npm i json-server
+- npm install @ant-design/icons --save
+
+---
+
+![News Portal Details](PH_News_Portal Details page.png)
+
+![News Portal Landing page](PH_News_Portal Landing page.png)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,21 @@
 import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
-import Banner from "@/components/UI/Banner";
+// import Banner from "@/components/UI/Banner";
+import AllNews from "@/components/UI/AllNews";
+import { useGetNewsesQuery } from "@/redux/api/api";
+import dynamic from "next/dynamic";
+
+const HomePage = ({ allNews }) => {
+  // console.log(allNews);
+  const { data, isLoading, isError, error } = useGetNewsesQuery(); //-> redux store data
+  // console.log(data);
 
 
-const HomePage = () => {
+  const DynamicBanner = dynamic(() => import("@/components/UI/Banner"), {
+    loading: () => <h1>Loading...</h1>,
+    ssr: false,
+  });
+
   return (
     <>
       <Head>
@@ -15,7 +27,8 @@ const HomePage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Banner />
+      <DynamicBanner />
+      <AllNews allNews={allNews} />
     </>
   );
 };
@@ -24,3 +37,30 @@ export default HomePage;
 HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
 };
+
+export const getStaticProps = async () => {
+  // const res = await fetch("http://localhost:3000/api/news"); // internal API connected with mongoDB
+  const res = await fetch("http://localhost:5000/news"); // --> json server
+  const data = await res.json();
+  // console.log(data);
+  return {
+    props: {
+      allNews: data,
+      // allNews: data.data, // when using internal API connected with mongoDB
+    },
+    revalidate: 10,
+  };
+};
+
+
+// for server side rendering (SSR) with json-server data
+/* export const getServerSideProps = async () => {
+  const res = await fetch("http://localhost:5000/news");
+  const data = await res.json();
+  // console.log(data);
+  return {
+    props: {
+      allNews: data,
+    }
+  };
+}; */
